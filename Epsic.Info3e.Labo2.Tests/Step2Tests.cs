e2d3daa5fb95ba3f5d09cf838731f3c9ea312ca3
Selmir <git@hajruli.ch>
2020-09-09 01:01:32 +0200

    Step 2 end

---
+++ Epsic.Info3e.Labo2.Tests/Step2Tests.cs
@@ -0,0 +1,58 @@
+// namespace Epsic.Info3e.Labo2.Tests
+// {
+//     using System.Collections.Generic;
+//     using System.Linq;
+//     using Epsic.Info3e.Labo2.Step2;
+//     using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//     [TestClass]
+//     public class Step2Tests
+//     {
+//         [TestMethod, TestCategory("Step2")]
+//         public void CatInheritsAnimal() 
+//         {
+//             var result = typeof(Cat).IsSubclassOf(typeof(Animal));
+//             Assert.AreEqual(true, result, "Cat n'hérite pas d'Animal.");
+//         }
+
+//         [TestMethod, TestCategory("Step2")]
+//         public void CowInheritsAnimal() 
+//         {
+//             var result = typeof(Cow).IsSubclassOf(typeof(Animal));
+//             Assert.AreEqual(true, result, "Cow n'hérite pas d'Animal.");
+//         }
+
+//         [TestMethod, TestCategory("Step2")]
+//         public void CatEats() 
+//         {
+//             var sut = new Cat("Domi");
+
+//             Assert.AreEqual("Je mange des croquettes.", sut.Eat());
+//         }
+
+//         [TestMethod, TestCategory("Step2")]
+//         public void CowEats() 
+//         {
+//             var sut = new Cow("Rose");
+
+//             Assert.AreEqual("Je mange du foin.", sut.Eat());
+//         }
+
+//         [TestMethod, TestCategory("Step2")]
+//         public void CatName() 
+//         {
+//             var sut = new Cat("Domi");
+
+//             Assert.AreEqual("Domi", sut.GetName());
+//         }
+
+//         [TestMethod, TestCategory("Step2")]
+//         public void CowName() 
+//         {
+//             var sut = new Cow("Rose");
+
+//             Assert.AreEqual("Holy Rose", sut.GetName());
+//         }
+//     }
+// }
+
