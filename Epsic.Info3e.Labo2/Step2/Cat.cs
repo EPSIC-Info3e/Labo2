3e6e6dabc4465aaf32d3fef0ccff97be8007d233
Laurent Keusch <25180858+laurentksh@users.noreply.github.com>
2020-09-10 10:38:35 +0200

    Correction des exercices

--- Epsic.Info3e.Labo2/Step2/Cat.cs
+++ Epsic.Info3e.Labo2/Step2/Cat.cs
@@ -1,6 +1,12 @@
 namespace Epsic.Info3e.Labo2.Step2
 {
-    public class Cat
+    public class Cat : Animal
     {
+        public Cat(string name_) : base(name_) 
+        {
+
+        }
+
+        public override string Eat() => "Je mange des croquettes.";
     }
 }
--- Epsic.Info3e.Labo2/Step2/Cow.cs
+++ Epsic.Info3e.Labo2/Step2/Cow.cs
@@ -1,6 +1,14 @@
 namespace Epsic.Info3e.Labo2.Step2
 {
-    public class Cow
+    public class Cow : Animal
     {
+        public Cow(string name_) : base(name_)
+        {
+
+        }
+
+        public override string Eat() => "Je mange du foin.";
+
+        public new string GetName() => $"Holy {Name}";
     }
 }
